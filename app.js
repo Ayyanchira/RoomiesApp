6cd7b9f20e3bfb8e0b84e91b6da3517c8d27b2c8
AkshayAyyanchira <aayyanch@uncc.edu>
2018-01-07 23:06:44 -0500

    Changing time to adjust Charlotte's time

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/submitTask',function(req,res){
     var jobname = req.body.jobname;
     var point = req.body.point;
     var date =  new Date();
-    date.setHours = date.getHours() - 5;
+    date.setHours = date.getHours() - 10;
     var dateAndTimeString = date.toLocaleDateString() +" "+ date.toLocaleTimeString();
     connection.query('insert into pointlog(username,jobid,jobname,point,datetime) values(?,?,?,?,?)',[username,jobid,jobname,point,dateAndTimeString], function (error, results, fields) {
     if (error) {
